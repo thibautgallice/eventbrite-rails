3caca51d432757e869cbbcd998271bcab341a1ca
thibautgallice <gt.thibaut@gmail.com>
2020-02-18 21:36:20 +0100

    heroku push

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,8 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+
+
   def index
     @events = Event.order('events.start_date ASC').all
   end
@@ -12,17 +16,15 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = Event.new(title:params['title'],description:params['description'],admin:current_user,start_date:params['start_date'],price:params['price'], location: params['location'], duration: params['duration'])
+    @user = current_user
+    @event = Event.new(title:params['title'],description:params['description'],admin:@user,start_date:params['start_date'],price:params['price'], location: params['location'], duration: params['duration'])
 
     if @event.save
       flash[:success] = "Event bien créé !"
       @success = "Bravo"
-      redirect_to root_path(:add_event => @success)
+      redirect_to @event
     else
       @fail = @event.errors.full_messages
-      puts "========================================================================================="
-      puts @event.errors.full_messages
-      puts "========================================================================================="
       render new_event_path
     end
   end
--- app/models/event.rb
+++ app/models/event.rb
@@ -7,7 +7,7 @@ class Event < ApplicationRecord
   validates :title, length: {minimum: 5, maximum: 140}
   validates :description, length: {minimum: 20, maximum: 1000}
   validates :price, numericality: {greater_than: 0, less_than_or_equal_to: 1000}
-  # validate :multiple_of_five
+  validate :multiple_of_five
   validate :cant_be_past
   validates :duration, :numericality => { :greater_than => 0 }
   
@@ -16,11 +16,14 @@ class Event < ApplicationRecord
   private 
 
   def multiple_of_five
-    errors.add(:duration, "has to be a multiple of 5") unless duration % 5 == 0 
+    unless duration.nil?
+      errors.add(:duration, "has to be a multiple of 5") unless duration % 5 == 0
+    end
   end
 
   def cant_be_past
-    errors.add(:start_date, "can't be past") if start_date < Time.now
+    unless start_date.nil?
+      errors.add(:start_date, "can't be past") if start_date < Time.now
+    end
   end
-  
 end
