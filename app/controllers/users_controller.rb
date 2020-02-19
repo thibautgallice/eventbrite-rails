a3a2614b1a582d34a92c68f8b2a592fb923385a0
thibautgallice <gt.thibaut@gmail.com>
2020-02-19 01:41:40 +0100

    final

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   before_action :is_current_user?, only: [:show]
   before_action :authenticate_user!
 
+  def index
+    redirect_to root_path
+  end
+
   def show
     @user = set_current_user
     @userevents = Event.order('events.start_date ASC').where(admin:@user)
@@ -13,6 +17,27 @@ class UsersController < ApplicationController
     end
   end
 
+
+def update_with_password(params, *options)
+    current_password = params.delete(:current_password)
+
+    if params[:password].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation) if params[:password_confirmation].blank?
+    end
+
+    result = if params[:password].blank? || valid_password?(current_password)
+      update_attributes(params, *options)
+    else
+      self.assign_attributes(params, *options)
+      self.valid?
+      self.errors.add(:current_password, current_password.blank? ? :blank : :invalid)
+      false
+    end
+
+    clean_up_passwords
+    result
+end
 
 
   private 
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "events#index"
-  resources :users, only: [:show]
+  get 'team', to: 'static#team'
+
+
+  resources :users, only: [:show, :index]
   resources :events, only: [:index, :new, :show, :create]
 end
